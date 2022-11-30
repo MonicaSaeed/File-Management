5dcf50d9199e2169754ca3170a0d113ccbdf7a26
Monica Saeed <monicasaeed12@gmail.com>
2022-11-30 23:06:40 +0200

    update printEmployee

--- ass1_file/main.cpp
+++ ass1_file/main.cpp
@@ -8,239 +8,10 @@
 
 using namespace std;
 
-template<class T>
-struct slNode
-{
-    T info;
-    slNode<T>* next;
-    slNode(T info, slNode<T>* Ptr = NULL)
-    {
-        this->info=info;
-        this->next=Ptr;
-    }
-};
-
-template<class T>
-class list
-{
-private:
-    slNode<T> *head, *tail;
-    int Size;
-public:
-    class iterator
-    {
-    private:
-        slNode<T> *nodePtr;
-    public:
-        iterator(slNode<T> *p = NULL)
-        {
-            nodePtr = p;
-        }
-        slNode<T>* getnodePtr()
-        {
-            return nodePtr;
-        };
-        iterator operator++(int num)
-        {
-            iterator tmp = *this;
-            nodePtr = nodePtr->next;
-            return tmp;
-        }
-        iterator operator++()
-        {
-            nodePtr = nodePtr->next;
-            return *this;
-        }
-        iterator operator+=(int num)
-        {
-            iterator *tmp = this;
-            while (num--)
-            {
-                nodePtr = nodePtr->next;
-            }
-            return *this;
-        }
-        T& operator*()
-        {
-            return nodePtr->info;
-        }
-        bool operator==(const iterator& Node)
-        {
-            return (nodePtr == Node.nodePtr);
-        }
-        bool operator!=(const iterator& Node)
-        {
-            return (nodePtr != Node.nodePtr);
-        }
-    };
-
-
-    list()
-    {
-        head = tail = NULL;
-        Size = 0;
-    }
-
-    ~list()
-    {
-        //deallocate();
-    }
-
-
-    void push_front(T info)
-    {
-        slNode<T> *Ptr = new slNode<T>(info);
-        if(!head)
-        {
-            head = tail = Ptr;
-        }
-        else
-        {
-            Ptr->next=head;
-            head=Ptr;
-        }
-        Size++;
-    }
-    void push_back(T info)
-    {
-        slNode<T> *Ptr = new slNode<T>(info);
-        if(!head)
-        {
-            head = tail = Ptr;
-        }
-        else
-        {
-            tail->next=Ptr;
-            tail=Ptr;
-        }
-        Size++;
-    }
-    void insert(iterator loc, T info)
-    {
-        slNode<T>* Ptr = head;
-        if (loc.getnodePtr()==tail)
-        {
-            push_back(info);
-        }
-        else
-        {
-            while(Ptr&&Ptr->next!=loc.getnodePtr())
-            {
-                Ptr = Ptr->next;
-            }
-            Ptr->next = new slNode<T>(info);
-            Ptr->next->next = loc.getnodePtr();
-            Size++;
-        }
-    }
-    void pop_front()
-    {
-        slNode<T>* Ptr=head;
-        if(head==tail)
-        {
-            head=tail=NULL;
-        }
-        else
-        {
-            head=head->next;
-        }
-        delete Ptr;
-        Size--;
-    }
-    void pop_back()
-    {
-        slNode<T>* Ptr=head;
-        if(head==tail)
-        {
-            head=tail=NULL;
-        }
-        else
-        {
-            while(Ptr&&Ptr->next!=tail)
-            {
-                Ptr = Ptr->next;
-            }
-            tail = Ptr;
-            Ptr = Ptr->next;
-            tail->next = NULL;
-        }
-        delete Ptr;
-        Size--;
-    }
-    iterator remove(iterator loc)
-    {
-        slNode<T>* Ptr = head;
-        if (loc.getnodePtr()==tail)
-        {
-            pop_back();
-        }
-        else
-        {
-            while(Ptr&&Ptr->next!=loc.getnodePtr())
-            {
-                Ptr = Ptr->next;
-            }
-            Ptr->next = loc.getnodePtr()->next;
-            Size--;
-        }
-        delete loc.getnodePtr();
-        return iterator(Ptr);
-    }
-    iterator search(T info)
-    {
-        slNode<T>* Ptr = head;
-        while(Ptr&&Ptr->info!=info)
-        {
-            Ptr = Ptr->next;
-        }
-        return iterator(Ptr);
-    }
-    void clear()
-    {
-        slNode<T> *tmp;
-        while(head != NULL)
-        {
-            tmp = head;
-            head = head->next;
-            delete tmp;
-        }
-        Size = 0;
-    }
-    int size()
-    {
-        return Size;
-    }
-    T front()
-    {
-        return head->info;
-    }
-    T back()
-    {
-        return tail->info;
-    }
-    iterator begin()
-    {
-        return iterator(head);
-    }
-    iterator end()
-    {
-        return iterator(tail->next);
-    }
-    iterator rbegin()
-    {
-        return iterator(tail);
-    }
-    iterator rend()
-    {
-        slNode<T> tmp(0,head);
-        return iterator(tmp);
-    }
-};
-
 void addEmployees(fstream &fileName,fstream &f2,string EID, string DID, string EName, string EPosition);
 int totalRecordsSize=0;
 void addEPrimaryIndex(fstream &EPrimaryIndex, string id,int li);
-string printEmployee(fstream &EPrimaryIndex, string id);
+pair<int,int> printEmployee(fstream &EPrimaryIndex, string id);
 
 
 /*long BinarySearch(fstream f, string ID)
@@ -257,9 +28,12 @@ int main()
     Department.open("Department.txt",ios::in | ios::out| ios::app);
     EPrimaryIndex.open("EPrimaryIndex.txt",ios::in | ios::out| ios::app);
 
-    //addEmployees(Employees,EPrimaryIndex,"2","i","m","s");
-    //addEmployees(Employees,EPrimaryIndex,"30","monica","is","student");
-    cout<<printEmployee(EPrimaryIndex,"30");
+//    addEmployees(Employees,EPrimaryIndex,"2","i","m","s");
+//    addEmployees(Employees,EPrimaryIndex,"30","monica","is","student");
+   pair<int,int> p= printEmployee(EPrimaryIndex,"30");
+   cout<<p.first<<" "<<p.second<<endl;
+
+
 
 
     return 0;
@@ -271,34 +45,27 @@ void addEPrimaryIndex(fstream &EPrimaryIndex, string id,int li)
     EPrimaryIndex<<totalRecordsSize<<'&'<<id<<'&';
     totalRecordsSize+=li;
 }
-string printEmployee(fstream &EPrimaryIndex,string id){ //return byteofficet
-    vector <pair<string,string>>v(0);
-    EPrimaryIndex.seekg(0,ios::end);
-    int textLength = (int)EPrimaryIndex.tellg();
+
+//first int is the byte offset in employee file and second int is the byte offset in primarykey file
+pair<int,int> printEmployee(fstream &EPrimaryIndex,string id){
+    vector <pair<string,pair<int,int>>>v(0);
     EPrimaryIndex.seekg(0,ios::beg);
     int textStart =0;
 
-    while(textStart<textLength){
-        char temp1,temp2;
+    while(!EPrimaryIndex.eof()){
         string Sbo,SID;
-        EPrimaryIndex>>temp1;
-        while(temp1!='&')
-        {
-            Sbo+=temp1;
-            EPrimaryIndex>>temp1;
-        }
-        EPrimaryIndex>>temp2;
-        while(temp2!='&')
-        {
-            SID+=temp2;
-            EPrimaryIndex>>temp2;
-        }
-        textStart += (int)EPrimaryIndex.tellg();
-        pair<string,string> p(SID,Sbo);
-        v.push_back(p);
+        getline(EPrimaryIndex,Sbo,'&');
+        getline(EPrimaryIndex,SID);
+        if(Sbo=="" || SID=="")break;
+        pair<int,int> p(stoi(Sbo),textStart);
+        pair<string,pair<int,int>> p1(SID,p);
+        v.push_back(p1);
+        textStart ++;
     }
-    sort(v.begin(),v.end());
 
+    sort(v.begin(),v.end());
+    
+    //binary search
     int start = 0;
     int end = (int)v.size()-1;
     int mid = (start+end)/2;
@@ -319,9 +86,9 @@ string printEmployee(fstream &EPrimaryIndex,string id){ //return byteofficet
         }
         mid = (start+end)/2;
     }
-    if(start>end) //if not found
+    if(start>end)
     {
-        return "-1";
+        return {-1,-1};
     }
 
 }
